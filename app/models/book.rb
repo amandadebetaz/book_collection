7c5b2201a9b64b5c036302bd71f414d6c206624e
root <root@localhost>
2026-09-15 03:45:54 +0000

    Add user book associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
+    has_many :user_books
+	has_many :users, through: :user_books
+
     validates :title, presence: true
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_09_15_014513) do
+ActiveRecord::Schema[8.0].define(version: 2026_09_15_032706) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -28,6 +28,19 @@ ActiveRecord::Schema[8.0].define(version: 2026_09_15_014513) do
     t.text "description"
     t.integer "position"
     t.boolean "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_books", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
